b87b19c6772cefd9e743fc1a69f37cbd2ccab086
eclonit <davizinhozl.mendes@gmail.com>
2019-05-10 11:57:11 -0300

    comentario

---
+++ src/model/PessoaFisica.java
@@ -0,0 +1,19 @@
+package model;
+
+public class PessoaFisica extends Pessoa {
+private long cpf;
+
+public long getCpf() {
+	return cpf;
+	
+}
+
+public void setCpf (long cpf ) {
+	this.cpf = cpf;
+ }
+
+public void falar() { 
+	System.out.println(" Falando - Pessoa fisica");
+	
+}
+}
---
+++ src/model/PessoaJuridica.java
@@ -0,0 +1,17 @@
+package model;
+
+public class PessoaJuridica extends Pessoa{
+	private long cnpj;
+	
+	public long getCnpj () {
+		return cnpj;
+	}
+	
+	public void setCnpj (long cnpj) {
+		this.cnpj = cnpj;
+	}
+public void falar() { 
+	System.out.println(" Falando - Pessoa Juridica");
+	
+}
+}
